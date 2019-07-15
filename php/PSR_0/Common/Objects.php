9b27006accb4e328e2c383b55854f2bbd8c939cd
d.c <chengyue.wang@iservernetwork.com>
2019-07-15 22:16:36 +0800

    content: 	php的魔术方法 	1.__get,__set 访问或者设置一个对象不存在的属性会触发这两个函数 	2.__call,__callStatic:前者访问一个对象实例不存在的函数触发改函数;后者是访问类不存在的方法的时候会触发改函数的执行 	3.__toString():假如我们直接echo $object 这个对象,也就是直接认为这个$object是一个字符串的时候,这种对类型判断出问题的时候,会触发这个函数 	4.__invoke(): 如果我们直接$object("test3333333333333333"),也就是直接把对象实例当成一个函数的时候,会触发这个函数的执行

--- php/PSR_0/Common/Objects.php
+++ php/PSR_0/Common/Objects.php
@@ -5,9 +5,60 @@ namespace Common;
 class Objects
 {
 
+    protected  $array=[];
+
     static function test()
     {
         echo __NAMESPACE__.__FUNCTION__.PHP_EOL;
+    }
+
+    function __set($key,$value){
+
+        var_dump(__FUNCTION__);
+
+        $this->array[$key]=$value;
+    }
+
+
+    function __get($key){
+        var_dump(__FUNCTION__);
+
+        return $this->array[$key];
+
+    }
+
+    function __call($func,$param){
+
+        var_dump($func,$param);
+        var_dump(__FUNCTION__);
+    }
+
+    static function __callStatic($name, $arguments)
+    {
+        // TODO: Implement __callStatic() method.
+        var_dump($name);
+
+        var_dump($arguments);
+
+
+    }
+
+    function __toString()
+    {
+        // TODO: Implement __toString() method.
+        var_dump(__METHOD__);
+
+        return __CLASS__.PHP_EOL;
+
+    }
+    //将一个对象作为一个函数去访问
+
+    function __invoke($params)
+    {
+        // TODO: Implement __invoke() method.
+        var_dump(__METHOD__);
+        var_dump($params);
+
     }
 }
 //不能有echo之类的代码
--- php/PSR_0/index.php
+++ php/PSR_0/index.php
@@ -20,7 +20,34 @@ $db->order("id desc ");
 $db->limit(" 10 ");*/
 //链试方式
 //这是解决练市访问的关键
-$db->where("id = 3")->where("name = 3")->order("id desc")->limit(11)
+$db->where("id = 3")->where("name = 3")->order("id desc")->limit(11);
+
+$object = new Common\Objects();
+
+//访问一个对象不存在的属性
+//$object->title = "成越";
+
+//echo $object->title.PHP_EOL;
+
+//调用一个不存在的函数
+
+//$object -> getTest("hello ",array(3,32432,342432));
+//会显示方法不存在的错误提示
+
+//调用一个不存在的静态方法
+
+//Common\Objects::getFun("hello abc",array(1,3,34));
+//将一个对象以字符串的形式输出
+var_dump($object);
+
+echo $object;
+
+echo $object("test1222222222222222");
+
+
+
+die;
+
 Common\Objects::test();
 
 
