d11d60a98d419b6dc9cafdfa813fb82018ccd82d
D.C <chengyue.wang@iservernetwork.com>
2019-07-25 10:12:29 +0800

    content: 	1.增加了变量的copy and write 写时复制脚本zval.php 了解php的zval以及zval_value结构 	2.增加了常量变量，以及函数变量，以及引用 	3.数组结构的分析脚本

---
+++ php/php7.0/debug/ref.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = "string";
+
+
+$b = &$a;
+
+echo $a;
+
+echo $b;
+
+$b = "hello !!!!!!!!!!!";
+
+echo $a;
+
+echo $b;
+
+
+unset($b);
+echo $b;
+
+echo $a;
--- php/php7.0/debug/zval.php
+++ php/php7.0/debug/zval.php
@@ -1,5 +1,28 @@
 <?php
-$a = 3;
+$a = 2 ;
 echo $a;
+$b = 1.1 ;
+
+echo $b;
+
+$c = null;
+
+echo $c;
+$d = true;
+echo $d;
+$e = false;
+echo $e;
+$f = "string";
+
+echo $f;
+
+$g = [1,2,3];
+echo $g;
+
+$h = new stdClass();
+
+echo $h;
+
+
 
 
